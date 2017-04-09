418f04134d9205be6e24dfbba9a6e1f169ae7987
alex-power <powera17@hotmail.com>
2017-04-09 13:25:39 -0400

    Account Register/Account Controller

---
+++ RestaurantManagementProject/Auth/CustomPrincipal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Security.Principal;
+
+namespace CollegeEventWebsite.Auth
+{
+    public class CustomPrincipal : ICustomPrincipal
+    {
+        public IIdentity Identity { get; private set; }
+
+        public CustomPrincipal(string username)
+        {
+            this.Identity = new GenericIdentity(username);
+        }
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public UserType Type { get; set; }
+
+        public bool IsInRole(string role)
+        {
+            return role.Equals(Type);
+        }
+    }
+}
--- RestaurantManagementProject/Controllers/AccountController.cs
+++ RestaurantManagementProject/Controllers/AccountController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CollegeEventWebsite.Auth;
+using RestaurantManagementProject.Models.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
 
 namespace RestaurantManagementProject.Controllers
 {
@@ -19,7 +23,28 @@ namespace RestaurantManagementProject.Controllers
         [HttpPost]
         public ActionResult Login(string username, string password)
         {
-            return View();
+            User user = db.Users.FirstOrDefault(x => x.Username == username);
+            
+            if (user == null || user.Password != password)
+                return View();
+            else
+            {
+                CustomPrincipalSerializeModel serializeModel = new CustomPrincipalSerializeModel();
+                serializeModel.Id = user.Id;
+                serializeModel.Username = user.Username;
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+                string userData = serializer.Serialize(serializeModel);
+
+                FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(1, username, DateTime.Now, DateTime.Now.AddMinutes(30), true, userData);
+
+                string encTicket = FormsAuthentication.Encrypt(authTicket);
+                HttpCookie faCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket);
+                Response.Cookies.Add(faCookie);
+
+                return RedirectToAction("Index");
+            }
         }
 
         [HttpGet]
@@ -31,7 +56,27 @@ namespace RestaurantManagementProject.Controllers
         [HttpPost]
         public ActionResult Register(string username, string password, string email, string name)
         {
-            return View();
+            User user = new RestaurantManagementProject.User();
+            user.Username = username;
+            user.Password = password;
+            user.Email = email;
+            user.Name = name;
+            user.CreationDate = DateTime.Now;
+            
+
+            db.Users.Add(user);
+
+            Users_Customer cust = new Users_Customer();
+            cust.Id = user.Id;
+            
+            db.Users_Customer.Add(cust);
+
+            db.Database.Connection.Open();
+            db.SaveChanges();
+            db.Database.Connection.Close();
+
+
+            return RedirectToAction("Index");
         }
     }
 }
